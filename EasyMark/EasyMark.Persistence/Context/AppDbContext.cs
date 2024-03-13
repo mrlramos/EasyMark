ac688ff2010ddc6414a3a45f11c0ff813ad8cc89
Marlon Ramos <marlon_raoni10@hotmail.com>
2024-03-13 20:57:05 -0300

    Create DbContext and Repositories

---
+++ EasyMark/EasyMark.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EasyMark.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyMark.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ EasyMark/EasyMark.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using EasyMark.Domain.Entities;
+using EasyMark.Domain.Interfaces;
+using EasyMark.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyMark.Persistence.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity 
+    {
+        protected readonly AppDbContext Context;
+
+        public BaseRepository(AppDbContext context)
+        {
+            Context = context;
+        }
+
+        public void Create(T entity)
+        {
+            entity.DateCreated = DateTimeOffset.UtcNow;
+            Context.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            entity.DateUpdated = DateTimeOffset.UtcNow;
+            Context.Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            entity.DateDeleted = DateTimeOffset.UtcNow;
+            Context.Remove(entity);
+        }
+
+        public async Task<T> Get(Guid id, CancellationToken cancellationToken)
+        {
+            return await Context.Set<T>().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+
+        public async Task<List<T>> GetAll(CancellationToken cancellationToken)
+        {
+            return await Context.Set<T>().ToListAsync(cancellationToken);
+        }
+    }
+}

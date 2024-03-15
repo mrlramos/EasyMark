cc78dd746506a71f15f9d0651a2a423456641e69
Marlon Ramos <marlon_raoni10@hotmail.com>
2024-03-14 21:40:13 -0300

    Add UnitOfWork repository and ServiceExtensions

---
+++ EasyMark/EasyMark.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using EasyMark.Domain.Interfaces;
+using EasyMark.Persistence.Context;
+
+namespace EasyMark.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Commit(CancellationToken cancellationToken)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ EasyMark/EasyMark.Persistence/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using EasyMark.Domain.Interfaces;
+using EasyMark.Persistence.Context;
+using EasyMark.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyMark.Persistence
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigurePersistenceApp(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var connectionString = configuration.GetConnectionString("Sqlite");
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connectionString));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
